1dad88d35413ac696ce63e0e81a0e758955118bc
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-05 17:41:49 +0200

    fin de la leccion

---
+++ src/componentes/counterapptarea/counterapp.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './counterapp.sass';
+
+interface counterif {contador:number};
+export const counter = ({contador}:counterif) => {
+
+    const [ contadorhook , setCounter ] = useState(contador)
+    const sc = (arg:string) => {
+        switch(arg){
+            case '+': setCounter(contadorhook+1);break;
+            case '-': setCounter(contadorhook-1);break;
+            case 'R': setCounter(contador);break;
+        }
+    };
+
+    return(
+        <div className='capp' >
+            <h1>Counterapp</h1>
+            <h2>{contadorhook}</h2>
+            <button onClick={ () => {sc('+')} }>+1</button>
+            <button onClick={ () => {sc('-')} }>-1</button>
+            <button onClick={ () => {sc('R')} }>Reset</button>
+        </div>
+    )
+
+    /*
+    return(
+        <>
+            <h1>Counterapp</h1>
+            <h2>{contador}</h2>
+            <button onClick={ handlevent }></button>
+        </>
+    )
+    */
+    
+}
