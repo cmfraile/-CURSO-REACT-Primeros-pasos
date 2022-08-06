f6f12b8874b29e52dca4ce7a99dfc3e2b5f23c9b
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-06 20:14:48 +0200

    61

---
+++ tests/componentes/tsfragment.test.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+import { tsfragment as Tsf } from '../../src/componentes/fragment';
+
+const main = () => {
+    test('debe de hacer match de snapshot',() => {
+        render(<Tsf/>)
+    });
+}
+
+describe('Pruebas de <TSF>',main);
